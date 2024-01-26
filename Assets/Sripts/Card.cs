2aa23e7583c0ddc6cb1d6e38cff502054973d555
IgorM213 <igor.mijanovic02@gmail.com>
2024-01-27 00:45:03 +0100

    dmg metoda gotova

--- Assets/Sripts/Card.cs
+++ Assets/Sripts/Card.cs
@@ -2,23 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Card : MonoBehaviour
 {
-    [SerializeField] string tekstKarte { get; set; }  
-    [SerializeField] Sprite izgledKarte { get; set; }
-    [SerializeField] Dictionary<string, int> statoviKarte { get; set; }
-    [SerializeField] bool mozeDaSeMerguje { get; set; }  
+    [SerializeField] string tekstKarte { get; set; }
+    [SerializeField] float kevaDmg ;
+    [SerializeField] float redditDmg;
+    [SerializeField] float animeDmg;
+    [SerializeField] float dadJokeDmg;
+    [SerializeField] Sprite izgledKarte;
+    //private Dictionary<string, int> statoviKarte { get; set; }
+    [SerializeField] bool mozeDaSeMerguje;  
 
+    //private void napraviMapu(int keva, int reddit, int anime, int dadJoke)
+    //{
+    //    statoviKarte = new Dictionary<string, int>();
+    //    statoviKarte.Add("keva", keva);
+    //    statoviKarte.Add("reddit", reddit);
+    //    statoviKarte.Add("anime", anime);
+    //    statoviKarte.Add("dadJoke", dadJoke);
 
+    //}
+    
     // Start is called before the first frame update
+    public Card(float a, float b, float c, float d)
+    {
+        this.kevaDmg = a;
+        this.redditDmg = b;
+        this.animeDmg = c;
+        this.dadJokeDmg = d;
+    }
+
     void Start()
     {
         
+        //napraviMapu(kevaDmg, redditDmg, animeDmg, dadJokeDmg);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public float vratiKevaDmgStat()
+    {
+        return this.kevaDmg;
+    }
+    public float vratiRedditDmgStat()
+    {
+        return this.redditDmg;
+    }
+    public float vratiAnimeDmgStat()
+    {
+        return this.animeDmg;
+    }
+    public float vratiDadjokeDmgStat()
+    {
+        return this.dadJokeDmg;
+
     }
 }
--- Assets/Sripts/Player.cs
+++ Assets/Sripts/Player.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
+    [SerializeField] List<Card> dekKarata { get; set; }
+    [SerializeField] Card[] cards;
+    
     // Start is called before the first frame update
     void Start()
     {
